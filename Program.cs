e71ff0a6ee3ff9d921a2523423dff03666d48535
Maisa Rissi <maisarissi@microsoft.com>
2023-03-02 12:56:03 -0300

    send and delete message

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 ﻿using Azure.Identity;
 using Microsoft.Graph;
+using Microsoft.Graph.Me.SendMail;
 using Microsoft.Graph.Models;
+using Microsoft.Graph.Models.ODataErrors;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
         var settings = Settings.LoadSettings();
-        string[] scopes = {"User.Read", "Mail.Read"};
+        string[] scopes = {"User.Read", "Mail.Read", "Mail.ReadWrite", "Mail.Send"};
         GraphServiceClient graphClient = new Authentication().getGraphClient(scopes, settings);
 
         User? me = await graphClient.Me.GetAsync(
@@ -18,7 +20,7 @@ public class Program
         );
 
         Console.WriteLine($"Hello {me?.GivenName}!"); //givenName is null because this properties wasn't requested
-        Console.WriteLine($"$Hello {me.DisplayName}! Your email is {me?.Mail}");
+        Console.WriteLine($"$Hello {me?.DisplayName}! Your email is {me?.Mail}");
 
         //Get the first page of messages - the default page size is 10
         //More info: https://learn.microsoft.com/en-us/graph/api/user-list-messages?view=graph-rest-1.0&tabs=csharp#request
@@ -29,7 +31,7 @@ public class Program
             }*/
         );
 
-        Console.WriteLine($"Fetched {firstPage.Value.Count} messages via default request");
+        Console.WriteLine($"Fetched {firstPage?.Value?.Count} messages via default request");
 
         var messagesCollected = new List<Message>();
         
@@ -54,6 +56,80 @@ public class Program
 
         // Get the messages data;
         Console.WriteLine($"Fetched {messagesCollected.Count} messages via page iterator");
+        Console.WriteLine($"First message subject: {messagesCollected[0].Subject}");
+        Console.WriteLine($"Last message subject: {messagesCollected[messagesCollected.Count - 1].Subject}");
         Console.WriteLine("-----------Done with paged requests-----------");
+        
+        //Sending a message
+        var message = new Message
+        {
+            Subject = "Hello from Graph!",
+            Body = new ItemBody{
+                Content = "Hello from Graph!",
+                ContentType = BodyType.Text
+            },
+            ToRecipients = new List<Recipient>{
+                new Recipient{
+                    EmailAddress = new EmailAddress{
+                        Address = "admin@M365x18467905.onmicrosoft.com"
+                    }
+                }
+            }
+        };
+
+        var saveToSentItems = true;
+
+        var body = new SendMailPostRequestBody 
+        {
+            Message = message,
+            SaveToSentItems = saveToSentItems
+        };
+
+        try
+        {
+            await graphClient.Me.SendMail.PostAsync(body);
+            Console.WriteLine("Message sent!");
+        }
+        catch (ODataError odataError)
+        {
+            Console.WriteLine(odataError.Error.Code);
+            Console.WriteLine(odataError.Error.Message);
+            throw;
+        }
+
+        var lastMessage = await graphClient.Me.Messages.GetAsync(
+            requestConfig => {
+                requestConfig.QueryParameters.Top = 1;
+            }
+        );
+
+        Console.WriteLine("lastMessage: " + lastMessage.Value[0].Subject);
+        Console.WriteLine("lastMessage: " + lastMessage.Value[0].Id);
+
+        WaitCallback waitCallback = new WaitCallback((state) => {
+            Thread.Sleep(5000);
+            Console.WriteLine("WaitCallback: " + state);
+        });
+
+        try
+        {
+            await graphClient.Me.Messages[lastMessage.Value[0].Id].DeleteAsync();
+            Console.WriteLine("Message deleted!");
+        }
+        catch (ODataError odataError)
+        {
+            Console.WriteLine(odataError.Error.Code);
+            Console.WriteLine(odataError.Error.Message);
+            throw;
+        }
+
+        var lastMessageAfterDeleting = await graphClient.Me.Messages.GetAsync(
+            requestConfig => {
+                requestConfig.QueryParameters.Top = 1;
+            }
+        );
+
+        Console.WriteLine("lastMessageAfterDeleting: " + lastMessageAfterDeleting.Value[0].Subject);
+
     }
 }
